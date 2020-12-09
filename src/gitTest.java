ed7b1e727f071f3c67f0d12c54a2583f94d7b63f
zhangjie <550048625@qq.com>
2020-12-09 22:59:05 +0800

    版本2stash暂存起来之后做的新变化

--- src/gitTest.java
+++ src/gitTest.java
@@ -9,6 +9,10 @@ public class gitTest {
         System.out.println("git test  分支1.0版本");
 
         System.out.println("git test  分支2.0版本");
+
+
+        System.out.println("版本2stash暂存起来之后做的新变化");
+
     }
 
 
